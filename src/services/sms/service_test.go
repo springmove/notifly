bfa30944a2e56910459ca4e9f48cc43a05d6e3a6
liuyuntao <liuyuntao1221@163.com>
2021-06-22 16:20:15 +0800

    增加华为云发送短信接口

--- src/services/sms/service_test.go
+++ src/services/sms/service_test.go
@@ -9,22 +9,30 @@ import (
 
 var endpoint = "ashibro"
 var endpointCN = "ashibro_cn"
+var endpointHuawei = "huawei"
 
 func getService() *Service {
 	sms := Service{cfg: Config{Endpoints: map[string]base.Endpoint{
-		endpoint: {
-			Provider:  base.Twilio,
-			AppKey:    "ACc1921dda2551f493a630645923b24d17",
-			AppSecret: "b218b891ef85c68b48ee53d84b9c8b42",
-			HostNum:   "+12055761160",
-		},
-		endpointCN: {
-			Provider:     base.RongCloud,
+		//endpoint: {
+		//	Provider:  base.Twilio,
+		//	AppKey:    "ACc1921dda2551f493a630645923b24d17",
+		//	AppSecret: "b218b891ef85c68b48ee53d84b9c8b42",
+		//	HostNum:   "+12055761160",
+		//},
+		//endpointCN: {
+		//	Provider:     base.RongCloud,
+		//	AppKey:       "",
+		//	AppSecret:    "",
+		//	Region:       "86",
+		//	SignName:     "Ashibro",
+		//	TemplateCode: "bT0pus3s4CD8w4LrgZ_g2e",
+		//},
+		endpointHuawei: {
+			Provider:     base.Huawei,
 			AppKey:       "",
 			AppSecret:    "",
-			Region:       "86",
-			SignName:     "Ashibro",
-			TemplateCode: "bT0pus3s4CD8w4LrgZ_g2e",
+			TemplateCode: "",
+			HostNum:      "",
 		},
 	}}}
 
@@ -39,16 +47,18 @@ func TestService(t *testing.T) {
 	//if err := sms.Send(base.Request{
 	//	Endpoint: endpoint,
 	//	Mobile:   "+8618049956365",
-	//	Content:  "ashibro test sms2",
+	//	Content: map[string]string{
+	//				"code": "1234",
+	//			},
 	//}); err != nil {
 	//	fmt.Println(err.Error())
 	//}
 
 	if err := sms.Send(base.Request{
-		Endpoint: endpointCN,
-		Mobile:   "18621182783",
+		Endpoint: endpointHuawei,
+		Mobile:   "11111111111",
 		Content: map[string]string{
-			"code": "1234",
+			"code": "321456",
 		},
 	}); err != nil {
 		fmt.Println(err.Error())
